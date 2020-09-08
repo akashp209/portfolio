3b4357b0829ba70068611ea6fb953da7d08b7768
akashp209 <akashp209@gmail.com>
2020-09-08 14:33:35 +0530

    Footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -24,7 +24,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; All credit goes to Clever Programmer - Copyright 2020 Clever Programmer</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
         </div>
